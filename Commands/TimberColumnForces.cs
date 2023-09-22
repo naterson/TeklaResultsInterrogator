6aeaa2f87f3750e2666ed3d408aee235a7917c7c
naterson <77845855+naterson@users.noreply.github.com>
2023-09-22 18:33:48 -0400

    TimberColumnForces PRELIMINARY

---
+++ Commands/TimberColumnForces.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using TeklaResultsInterrogator.Core;
+using TSD.API.Remoting.Loading;
+using TSD.API.Remoting.Solver;
+using TSD.API.Remoting.Structure;
+using TSD.API.Remoting.Sections;
+
+namespace TeklaResultsInterrogator.Commands
+{
+    internal class TimberColumnForces : ForceInterrogator
+    {
+        public TimberColumnForces()
+        {
+            HasOutput = true;
+            RequestedMemberType = new List<MemberConstruction>() { MemberConstruction.TimberColumn };
+        }
+
+        public override async Task ExecuteAsync()
+        {
+            // Initialize parents
+            await InitializeAsync();
+
+            // Check for null properties
+            if (Flag)
+            {
+                return;
+            }
+
+            // Data setup and diagnostics initialization
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            int bufferSize = 65536 * 2;
+
+            // Unpacking loading data
+            FancyWriteLine("Loading Summary:", TextColor.Title);
+            Console.WriteLine("Unpacking loading data...");
+            Console.WriteLine($"{AllLoadcases.Count} loadcases found, {SolvedCases.Count} solved.");
+            Console.WriteLine($"{AllCombinations.Count} load combinations found, {SolvedCombinations.Count} solved.");
+            Console.WriteLine($"{AllEnvelopes.Count} load envelopes found, {SolvedEnvelopes.Count} solved.\n");
+
+            stopwatch.Stop();
+            List<ILoadingCase> loadingCases = AskLoading(SolvedCases, SolvedCombinations, SolvedEnvelopes);
+            bool reduced = AskReduced();
+            stopwatch.Start();
+
+            // Unpacking member data
+            FancyWriteLine("\nMember summary:", TextColor.Title);
+            Console.WriteLine("Unpacking member data...");
+
+            List<IMember> timberColumns = AllMembers.Where(c => RequestedMemberType.Contains(c.Data.Value.Construction.Value)).ToList();
+
+            Console.WriteLine($"{AllMembers.Count} structural members found in model.");
+            Console.WriteLine($"{timberColumns.Count} timber columns found.");
+
+            double timeUnpack = Math.Round(stopwatch.Elapsed.TotalSeconds, 3);
+            Console.WriteLine($"Loading and member data unpacked in {timeUnpack} seconds.\n");
+
+            // Organizing column stacks into lists of lifts
+            double startStack = timeUnpack;
+            FancyWriteLine("Organizing Column Stacks...", TextColor.Title);
+            //List<List<List<IMemberSpan>>> timberColumnsByLift = new List<List<List<IMemberSpan>>>();
+            //foreach (IMember column in timberColumns)
+            //{
+            //    List<Im>
+            //}
+
+
+
+            // Set up file
+            // TODO
+            double start1 = timeUnpack;
+            string file1 = SaveDirectory + @"TimberColumnForces_" + FileName + ".csv";
+            string header1 = String.Format("{0},{1},{2},{3},{4},{5},{6},{7},{8},{9},{10},{11},{12},{13},{14},{15},{16},{17},{18},{19},{20},{21}\n",
+                "Tekla GUID", "Member Name", "Level", "Section", "Breadth [in]", "Depth [in]", "Span Name",
+                "Start Node", "Start Node Fixity", "End Node", "End Node Fixity",
+                "Span Length [ft]", "Span Rotation [deg]", "Loading Name",
+                "Shear Major [k]", "Shear Minor [k]", "Moment Major [k-ft]", "Moment Minor [k-ft]",
+                "Axial Force [k]", "Torsion [k-ft]", "Deflection Major [in]", "Deflection Minor [in]");
+            File.WriteAllText(file1, "");
+            File.AppendAllText(file1, header1);
+
+            // Getting internal forces and writing table
+            FancyWriteLine("\nWriting internal forces table...", TextColor.Title);
+            using (StreamWriter sw1 = new StreamWriter(file1, true, Encoding.UTF8, bufferSize))
+            {
+                foreach (IMember member in timberColumns)
+                {
+                    string name = member.Name;
+                    Guid id = member.Id;
+                    IEnumerable<IMemberSpan> spans = await member.GetSpanAsync();
+                    Console.WriteLine($"\nColumn {name}:");
+
+                    int constructionPointIndex = member.MemberNodes.Value.First().Value.ConstructionPointIndex.Value;
+                    IEnumerable<IConstructionPoint> constructionPoints = await Model.GetConstructionPointsAsync(new List<int>() { constructionPointIndex });
+                    int planeId = constructionPoints.First().PlaneInfo.Value.Index;
+                    IEnumerable<IHorizontalConstructionPlane> level = await Model.GetLevelsAsync(new List<int>() { planeId });
+                    string levelName;
+                    if (level.Any())
+                    {
+                        levelName = level.First().Name;
+                    }
+                    else
+                    {
+                        levelName = "Not Associated";
+                    }
+
+                    foreach (IMemberSpan span in spans)
+                    {
+                        string spanName = span.Name;
+                        int spanIdx = span.Index;
+                        double length = span.Length.Value;
+                        double lengthFt = length * 0.00328084; // Converting from [mm] to [ft]
+                        double rot = Math.Round(span.RotationAngle.Value * 57.2958, 3); // Converting from [rad] to [deg]
+                        IMemberSection generalSection = (IMemberSection)span.ElementSection.Value;
+                        ITimberBeamSection section = (ITimberBeamSection)generalSection.PhysicalSection.Value;
+                        string sectionName = section.LongName;
+                        double breadth = Math.Round(section.Breadth * 0.0393701, 4);  // Converting from [mm] to [in]
+                        double depth = Math.Round(section.Depth * 0.0393701, 4);  // Converting from [mm] to [in]
+
+                        int startNodeIdx = span.StartMemberNode.ConstructionPointIndex.Value;
+                        string startNodeFixity = span.StartReleases.Value.DegreeOfFreedom.Value.ToString();
+                        if (span.StartReleases.Value.Cantilever.Value == true)
+                        {
+                            startNodeFixity += " (Cantilever end)";
+                        }
+                        startNodeFixity = startNodeFixity.Replace(',', '|');
+                        int endNodeIdx = span.EndMemberNode.ConstructionPointIndex.Value;
+                        string endNodeFixity = span.EndReleases.Value.DegreeOfFreedom.Value.ToString();
+                        if (span.EndReleases.Value.Cantilever.Value == true)
+                        {
+                            endNodeFixity += " (Cantilever end)";
+                        }
+                        endNodeFixity = endNodeFixity.Replace(',', '|');
+
+                        Console.WriteLine($"  Span {spanIdx}:");
+                        Console.WriteLine($"    Start: {startNodeFixity}");
+                        Console.WriteLine($"    End:   {endNodeFixity}");
+
+                        string spanLineOnly = String.Format("{0},{1},{2},{3},{4},{5},{6},{7},{8},{9},{10},{11},{12}",
+                            id, name, levelName, sectionName, breadth, depth, spanName,
+                            startNodeIdx, startNodeFixity, endNodeIdx, endNodeFixity, lengthFt, rot);
+
+                        //foreach (ILoadingCase loadingCase in loadingCases)
+                        //{
+                        //    string loadName = loadingCase.Name.Replace(',', '`');
+                        //    SpanResults spanResults = new SpanResults(span, 1, loadingCase, reduced, AnalysisType, member);
+
+                        //    // Getting maximum internal forces and displacements and locations
+                        //    MaxSpanInfo maxSpanInfo = await spanResults.GetMaxima();
+                        //    string maxLine = spanLineOnly + "," + String.Format("{0},{1},{2},{3},{4},{5},{6},{7},{8}",
+                        //        loadName,
+                        //        maxSpanInfo.ShearMajor.Value,
+                        //        maxSpanInfo.ShearMinor.Value,
+                        //        maxSpanInfo.MomentMajor.Value,
+                        //        maxSpanInfo.MomentMinor.Value,
+                        //        maxSpanInfo.AxialForce.Value,
+                        //        maxSpanInfo.Torsion.Value,
+                        //        maxSpanInfo.DeflectionMajor.Value,
+                        //        maxSpanInfo.DeflectionMinor.Value);
+                        //    sw1.WriteLine(maxLine);
+                        //}
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Core/ColumnLiftOrganizer.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using TeklaResultsInterrogator.Core;
+using TSD.API.Remoting.Loading;
+using TSD.API.Remoting.Solver;
+using TSD.API.Remoting.Structure;
+
+namespace TeklaResultsInterrogator.Core
+{
+    public class ColumnLifts
+    {
+        public IMember ParentMember { get; set; }
+        public List<NamedList<IMemberSpan>> Lifts { get; set; }
+
+        public ColumnLifts(IMember parentMember)
+        {
+            ParentMember = parentMember;
+            Lifts = new List<NamedList<IMemberSpan>>();
+
+            // Get all spans of member
+            // iterate through each span
+            // look at fixity of span
+            // bottom of span or top of previous span is pinned, add to new lift
+            //
+        }
+
+        public async Task OrganizeByFixity()
+        {
+            IEnumerable<IMemberSpan> spans = await ParentMember.GetSpanAsync();
+            spans = spans.OrderBy(s => s.Index);
+            bool previousSpanTopFixed = false;
+            foreach (var span in spans)
+            {
+                bool thisBotFixed = CheckFixity(span, StackEnd.Bottom);
+                bool thisTopFixed = CheckFixity(span, StackEnd.Top);
+                
+                if (!thisBotFixed || !previousSpanTopFixed)
+                {
+                    // if bottom of this span is NOT fixed, OR if top of previous span is NOT fixed, add this span to start of new lift and name the lift list
+                }
+                else
+                {
+                    // else add this span to end of previous lift
+                }
+
+                previousSpanTopFixed = thisTopFixed;
+            }
+
+            // sort each stack in each lift by stack index
+            // sort each lift in list by index of first stack in lift
+
+
+        }
+
+        private bool CheckFixity(IMemberSpan span, StackEnd end)
+        {
+            bool isFixed = false;
+            ISpanReleases? releases = null;
+            switch (end)
+            {
+                case StackEnd.Bottom:
+                    releases = span.StartReleases.Value;
+                    break;
+                case StackEnd.Top:
+                    releases = span.EndReleases.Value;
+                    break;
+                default:
+                    break;
+            }
+
+            if (releases != null)
+            {
+                string releaseValueString = releases.DegreeOfFreedom.Value.ToString();
+                if (releaseValueString.Contains("Mx") && releaseValueString.Contains("My") && releaseValueString.Contains("Mz"))
+                {
+                    isFixed = true;
+                }
+            }
+
+            return isFixed;
+        }
+
+        private enum StackEnd
+        {
+            Bottom,
+            Top,
+        }
+    }
+
+    public class NamedList<T>
+    {
+        public string Name { get; set; }
+        public List<T> Values { get; set; }
+
+        public NamedList(string name)
+        {
+            Name = name;
+            Values = new List<T>();
+        }
+
+        public void Add(T value)
+        {
+            Values.Add(value);
+        }
+    }
+}
